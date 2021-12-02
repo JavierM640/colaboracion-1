7ed80b9714992b25cd8b94e82bfd025d5278d208
trujoxx <ftrurue908@g.educaand.es>
2021-12-02 09:26:48 +0100

    Creación de la función del cálculo del área del rectángulo

--- colaboracion1.java
+++ colaboracion1.java
@@ -7,8 +7,18 @@ public class colaboracion1 {
         area = (b * a)/2;
 
         return area;
-    }    
+    }
+    public static double rectangulo(double b, double a){
+
+        double area;
+
+        area = (b * a);
+
+        return area;
+    }
+
     public static void main (String[] args) {
         System.out.println(triangulo(2, 3));
+        System.out.println(rectangulo(2,3));
     }
 }
