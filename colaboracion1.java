c56c99bd3310b184958e4d0dd03178b2b8f6274f
JavierM640 <fmaroje640@g.educaand.es>
2021-12-02 09:21:17 +0100

    Creación de la función triangulo para el calculo de su área

---
+++ colaboracion1.java
@@ -0,0 +1,14 @@
+public class colaboracion1 {
+
+    public static double triangulo(double b, double a){
+
+        double area;
+
+        area = (b * a)/2;
+
+        return area;
+    }    
+    public static void main (String[] args) {
+        System.out.println(triangulo(2, 3));
+    }
+}
