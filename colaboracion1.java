ead28478724b8d272f3138abc7f6e9ecaa18b99a
JavierM640 <fmaroje640@g.educaand.es>
2021-12-05 23:42:52 +0100

    Calcular area del circulo

--- colaboracion1.java
+++ colaboracion1.java
@@ -17,8 +17,18 @@ public class colaboracion1 {
         return area;
     }
 
+    public static double circulo(double r){
+
+        double area;
+
+        area = Math.PI * (r * r);
+
+        return area;
+    }
+
     public static void main (String[] args) {
         System.out.println(triangulo(2, 3));
         System.out.println(rectangulo(2,3));
+        System.out.println(circulo(3));
     }
 }
